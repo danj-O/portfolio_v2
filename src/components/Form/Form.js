78f26a3426ca5ac6b0884cd9e062b3499adcb60b
danj-O <danjowanjo@gmail.com>
2020-03-26 14:36:19 -0400

    honeypot!

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Form() {
   return (
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
