aa99f5f800ca2bd78bd2c735b8bdd54270f71625
danj-O <danjowanjo@gmail.com>
2020-03-26 13:34:42 -0400

    made analytics id hidden

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,17 +9,17 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-161924969-1",
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
+        // head: true,
         // Setting this parameter is optional
-        anonymize: true,
+        // anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        // respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
+        // pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
         // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
         // Enables Google Optimize Experiment ID
@@ -27,9 +27,9 @@ module.exports = {
         // Set Variation ID. 0 for original 1,2,3....
         // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
         // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "daniel-scott.netlify.com",
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "daniel-scott.netlify.com",
       },
     },
     `gatsby-plugin-react-helmet`,
